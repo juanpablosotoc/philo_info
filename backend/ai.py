55ad3aff6d2a2025f34e9b250c3bef15ccde3bf5
juanpablosotoc <117848358+juanpablosotoc@users.noreply.github.com>
2024-05-26 15:53:10 -0600

    added capacity to process youtube videos

--- backend/ai.py
+++ backend/ai.py
@@ -1,43 +1,58 @@
-from langchain.schema import HumanMessage, SystemMessage, AIMessage
-from langchain.chat_models import ChatOpenAI
+from openai import OpenAI
 import os
-# from langchain import prompts
 
 
 class Prompts:
-    @classmethod
-    def process_link_messages(cls, link: str) -> list:
+    @staticmethod
+    def process_link_messages(link: str) -> list:
         human_message_str = f"""
         Give me a detailed summary of what the following website is about:
         {link}
         """
-        human_message = HumanMessage(text=human_message_str)
-        return [human_message]
+        return [{"role": "user", "content": human_message_str}]
     
-    @classmethod
-    def process_text_messages(cls, text: str) -> list:
+    @staticmethod
+    def process_text_messages(text: str) -> list:
         human_message_str = f"""
         Give me a detailed summary of the text denoted by [[[ ]]]. 
         [[[ 
             {text} 
         ]]]
         """
-        human_message = HumanMessage(text=human_message_str)
-        return [human_message]
+        return [{"role": "user", "content": human_message_str}]
     
-    @classmethod
-    def process_document_messages(cls, file_path: str) -> list:
+    @staticmethod
+    def process_document_messages(file_path: str) -> list:
         messages = []
-
+        # ???????????????????????????????????????????????????????????? dont think we should do this,, rather split into images, handle docs, etc
         return messages
+    
+    @staticmethod
+    def key_frames_time_stamps_messages(transcript: str) -> list:
+        human_message_str = f"""
+        I am going to extract pictures from a video. I need the timestamps of the pictures that are most important. The text denoted by [[[ ]]] is the transcript of the video.
+        [[[ 
+            {transcript} 
+        ]]]
+        """
+        return [{"role": "user", "content": human_message_str}]
     
 class Chat(Prompts):
     def __init__(self) -> None:
         super().__init__()
-        self.chat_openai = ChatOpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"))
-    def ask(self, messages: list, **kwargs) -> str:
-        response = self.chat_openai(messages=[messages], **kwargs)
-        return response.generations[0][0].text
+        self.client = OpenAI()
+    def ask(self, messages: list) -> str:
+        stream = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=messages,
+            stream=True,
+        )
+        for chunk in stream:
+            if chunk.choices[0].delta.content is not None:
+                yield chunk.choices[0].delta.content
 
-    
 chat = Chat()
+# res = chat.ask(Prompts.process_link_messages(r"https://www.nhs.uk/medicines/risperidone/#:~:text=You'll%20usually%20start%20on,and%20half%20in%20the%20evening."))
+# print(res)
+# for i in res:
+#     print(i, end="")
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,48 @@
 from ai import chat, Prompts
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+from functools import partial
+from multiprocessing.pool import Pool
+import cv2
+import yt_dlp as youtube_dl
+
+class Video:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+
+    def get_key_frames_time_stamps(self, transcript: str):
+        # as of now it doesnt give very very relevant key frames
+        response = chat.ask(Prompts.key_frames_time_stamps_messages(transcript))
+        for message in response:
+            yield message
+        
+    def process_video_parallel(url, skip_frames, process_number):
+        cap = cv2.VideoCapture(url)
+        num_processes = os.cpu_count()
+        frames_per_process = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) // num_processes
+        cap.set(cv2.CAP_PROP_POS_FRAMES, frames_per_process * process_number)
+        x = 0
+        count = 0
+        while x < 10 and count < frames_per_process:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            filename =r"PATH\shot"+str(x)+".png"
+            x += 1
+            cv2.imwrite(filename.format(count), frame)
+            count += skip_frames  # Skip 300 frames i.e. 10 seconds for 30 fps
+            cap.set(1, count)
+        cap.release()
 
 
-class YoutubeVideo:
-    def __init__(self, link: str) -> None:
-        self.link = link
+class YoutubeVideo(Video):
+    def __init__(self, url: str) -> None:
+        self.video_url = url
+        self.video_id = url.split("v=")[1]
+        if self.video_id.find("&") != -1: 
+            self.video_id = self.video_id.split("&")[0]
+        if self.video_id.find("#") != -1:
+            self.video_id = self.video_id.split("#")[0]
         self.__key_frames = None
         self.__transcript = None
     
@@ -16,12 +55,45 @@ class YoutubeVideo:
     def transcript(self):
         if self.__transcript is None: self.get_transcript()
         return self.__transcript
+    
+    def get_transcript(self, languages: list[str] = ['en']):
+        """Languages is a list of language codes in a descending priority.(Only fetches one language)"""
+        self.__transcript = YouTubeTranscriptApi.get_transcript(self.video_id, languages)
 
-    def get_transcript(self):
-        pass
+    def download_specific_frames(self, time_stamps: list[str]):
+        ydl_opts = {}
+        ydl = youtube_dl.YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(self.video_url, download=False)
 
-    def get_key_frames(self):
-        pass
+        formats = info_dict.get('formats', None)
+
+        print("Obtaining frames")
+        for f in formats:
+            if f.get('format_note', None) == '144p':
+                url = f.get('url', None)
+                cpu_count = os.cpu_count()
+                with Pool(cpu_count) as pool:
+                    pool.map(partial(Video.process_video_parallel, url, 300), range(cpu_count))
+
+
+
+if __name__ == "__main__":
+    y = YoutubeVideo("https://www.youtube.com/watch?v=ehTIhQpj9ys")
+    video = Video("PATH")
+    y.download_specific_frames([])
+
+
+
+
+# yout = YoutubeVideo("ehTIhQpj9ys")
+# print(yout.transcript, type(yout.transcript), type(yout.transcript[0]))
+# key_frames_resp = Video('dejnjn').get_key_frames_time_stamps(yout.transcript)
+# for mess in key_frames_resp:
+#     print(mess, end="")
+
+
+
+
 
 class Info:
     def __init__(self, content: str, metadata_file_path:None|str=None) -> None:
